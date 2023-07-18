73e917a8dfb881efeaa821208b6ddb9ca8d94171
김건우 <kwforu123@naver.com>
2023-07-18 12:23:16 +0900

    초기상태 설정

---
+++ conf/auth.js
@@ -0,0 +1,8 @@
+const dbconf = {
+    host:'localhost',
+    user: 'root',
+    password: '343188',
+    database:'test'
+}
+
+module.exports = dbconf;
---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const mysql = require('mysql');
+const bp = require("body-parser");
+const cors = require("cors");
+
+const dbconf = require("./conf/auth.js")
+
+const app = express();
+const port = 9000;
+
+const db = mysql.createConnection(dbconf)
+db.connect(function (err) {
+    if (err) {
+        console.log("Database connection failed");
+        throw err;
+    }
+    console.log("Connected to the database");
+});
+
+app.use(bp.json());
+app.use(cors());
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/test', (req, res) => {
+    const sql = 'SELECT * FROM test_name';
+
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Server Error' });
+            return;
+        }
+        console.log(result);
+        res.json(result);
+    });
+});
+
+app.post('/test', (req, res) => {
+    const sql = 'INSERT INTO test_name(name) VALUES (?)';
+    const name = req.body.name
+
+    console.log(name);
+
+    db.query(sql, name,(err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Server Error' });
+            return;
+        }
+        res.json({success:true});
+    });
+});
+
+app.listen(port, () => {
+    console.log('App listening on port ' + port);
+});
