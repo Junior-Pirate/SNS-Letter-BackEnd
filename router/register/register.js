ef417f2beac023f3e8c928c019f1dcd5802a276f
김건우 <96743351+kimgunwooo@users.noreply.github.com>
2023-08-03 23:40:07 +0900

    회원가입 반환 처리

--- router/register/register.js
+++ router/register/register.js
@@ -10,7 +10,7 @@ router.use(bp.json());
 router.use(cors());
 
 router.post('/', async (req, res) => {
-    const sql = 'INSERT INTO user(email,name,pw) VALUES (?,?,?)';
+    const sql = 'SELECT email FROM user where email = ?'
     const {email,name,pw} = req.body
     params = [email,name,pw]
 
@@ -19,13 +19,24 @@ router.post('/', async (req, res) => {
     const hashPassword = await bcrypt.hash(params[2], salt)
     params[2] = hashPassword
 
-    db.query(sql, params,(err, result) => {
+    db.query(sql, params[0],(err, rows) => {
         if (err) {
             console.error(err);
             res.status(500).json({ error: 'Server Error' });
             return;
         }
-        res.json({success:true});
+        console.log(rows)
+        if(rows.length==0){
+            var sql = db.query('INSERT INTO user(email,name,pw) VALUES (?,?,?)',params,(err,result)=>{
+                if(err) throw err;
+                else{
+                    res.status(200).json({message:'로그인 성공'})
+                }
+            })
+        }
+        if(rows.length!=0){
+            res.status(401).json({message:'이메일이 중복되었습니다.'})
+        }
     });
 });
 
