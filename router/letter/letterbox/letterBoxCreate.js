e543eabd589ca01d1fc686c2cd684433605a8777
Kor-NOH <shehddnjs1982@naver.com>
2023-12-04 14:30:04 +0900

    already letterbox, move page

--- router/letter/letterbox/letterBoxCreate.js
+++ router/letter/letterbox/letterBoxCreate.js
@@ -19,7 +19,7 @@ const letterBoxCreate = async (req, res) => {
         }
 
         if (user.letterbox === true) {
-            return res.json({ LetterBoxCreate: false, message: "이미 편지함이 존재합니다" });
+            return res.status(250).json({ LetterBoxCreate: true, message: "이미 편지함이 존재합니다", userId : user.id });
         }
         
         //이걸 프론트엔드에서 처리한 뒤에 백엔드로는 userId를 보내줘야 할듯
